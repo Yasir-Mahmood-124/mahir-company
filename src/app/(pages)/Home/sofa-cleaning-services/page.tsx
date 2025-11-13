65b7c1ff1deab0203c2cd28c7eb880a402e2c80c
Faizan80235 <fa3031980@gmail.com>
2025-11-13 18:10:29 +0500

    Updated Sofa Cleaning Services page – added component

--- src/app/(pages)/Home/sofa-cleaning-services/page.tsx
+++ src/app/(pages)/Home/sofa-cleaning-services/page.tsx
@@ -4,11 +4,14 @@ import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
 import SofaCleaningBanner from "@/app/Components/sofa_cleaning_banner";
 import SofaCleaningDescription from "@/app/Components/sofa_cleaning_description";
+import SofaCleaningServicesSection from "@/app/Components/SofaCleaningServiceCard";
 export default function HandymanHome() {
   return (
     <div>
       <NavBar></NavBar>
       <SofaCleaningBanner></SofaCleaningBanner>
+
+      <SofaCleaningServicesSection></SofaCleaningServicesSection>
       <SofaCleaningDescription></SofaCleaningDescription>
       <Footer></Footer>
 
