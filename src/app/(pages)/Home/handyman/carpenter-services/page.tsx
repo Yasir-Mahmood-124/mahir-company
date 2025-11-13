268f66b321e823d00827492a6ee83850c60f6e88
Faizan80235 <fa3031980@gmail.com>
2025-11-14 01:52:07 +0500

    move carpenter-services folder into handyman folder

---
+++ src/app/(pages)/Home/handyman/carpenter-services/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import NavBar from "@/app/Components/Navabar";
+import Footer from "@/app/Components/HandymanFooter";
+import CarpanterServiceBanner from "@/app/Components/CarpanterServiceBanner";
+import CarpanterServicesSection from "@/app/Components/CarpanterServiceSection";
+import CarpanterDescription from "@/app/Components/CarpanterDescription";
+export default function HandymanHome() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <CarpanterServiceBanner></CarpanterServiceBanner>
+      <CarpanterServicesSection></CarpanterServicesSection>
+      <CarpanterDescription></CarpanterDescription>
+      <Footer></Footer>
+    </div>
+  );
+}
