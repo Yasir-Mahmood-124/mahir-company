bbc9576650b0c2384c6922c0bfc75684d4046eec
Faizan80235 <fa3031980@gmail.com>
2025-11-14 02:09:00 +0500

    move mattress-cleaning-services folder into cleaning-services folder

---
+++ src/app/(pages)/Home/cleaning-services/mattress-cleaning-services/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import NavBar from "@/app/Components/CleaningServiceNavbar";
+import Footer from "@/app/Components/HandymanFooter";
+import MattresCleaningService from "@/app/Components/MattresCLeaningServiceCard";
+import MattressCleaningBanner from "@/app/Components/mattress_cleaning_banner";
+import MattressCleaningDescription from "@/app/Components/mattress_cleaning_description";
+export default function HandymanHome() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <MattressCleaningBanner></MattressCleaningBanner>
+      <MattresCleaningService></MattresCleaningService>
+      <MattressCleaningDescription></MattressCleaningDescription>
+      <Footer></Footer>
+    </div>
+  );
+}
