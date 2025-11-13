73c940e96941381d448a1b56647d79ac994303a3
Faizan80235 <fa3031980@gmail.com>
2025-11-14 02:23:57 +0500

    create a beautician service folder

---
+++ src/app/(pages)/Home/beautician/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import BeauticianBanner from "@/app/Components/beautician-banner";
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+        <BeauticianBanner></BeauticianBanner>
+    </div>
+  );
+}
