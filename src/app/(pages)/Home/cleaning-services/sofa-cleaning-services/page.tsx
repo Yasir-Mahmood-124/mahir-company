03bfd9566f4623cc72b567bac6239e88d9ee9b8a
Faizan80235 <fa3031980@gmail.com>
2025-11-14 02:09:06 +0500

    move sofa-cleaning-services folder into cleaning-services folder

---
+++ src/app/(pages)/Home/cleaning-services/sofa-cleaning-services/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import NavBar from "@/app/Components/CleaningServiceNavbar";
+import Footer from "@/app/Components/HandymanFooter";
+import SofaCleaningBanner from "@/app/Components/sofa_cleaning_banner";
+import SofaCleaningDescription from "@/app/Components/sofa_cleaning_description";
+import SofaCleaningServicesSection from "@/app/Components/SofaCleaningServiceCard";
+export default function HandymanHome() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <SofaCleaningBanner></SofaCleaningBanner>
+
+      <SofaCleaningServicesSection></SofaCleaningServicesSection>
+      <SofaCleaningDescription></SofaCleaningDescription>
+      <Footer></Footer>
+
+    </div>
+  );
+}
