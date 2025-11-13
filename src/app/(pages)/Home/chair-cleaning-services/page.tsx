d03ae9e1be5cbddc98d0e1a1670f7616dd177678
Faizan80235 <fa3031980@gmail.com>
2025-11-13 17:21:59 +0500

    Updated Chair Cleaning Services page – added component

--- src/app/(pages)/Home/chair-cleaning-services/page.tsx
+++ src/app/(pages)/Home/chair-cleaning-services/page.tsx
@@ -2,6 +2,7 @@
 
 import ChairCleaningBanner from "@/app/Components/chair_cleaning_banner";
 import ChairCleaningDescription from "@/app/Components/chair_cleaning_description";
+import ChairCleaningServicesSection from "@/app/Components/ChairCleningServiceCard";
 import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
 export default function HandymanHome() {
@@ -9,6 +10,7 @@ export default function HandymanHome() {
     <div>
       <NavBar></NavBar>
       <ChairCleaningBanner></ChairCleaningBanner>
+      <ChairCleaningServicesSection></ChairCleaningServicesSection>
       <ChairCleaningDescription></ChairCleaningDescription>
       <Footer></Footer>
     </div>
