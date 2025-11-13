2274ae402bc4c030255151d3c0a594a9dfd47a4f
Faizan80235 <fa3031980@gmail.com>
2025-11-14 01:52:31 +0500

    move pest-control-services folder into handyman folder

---
+++ src/app/(pages)/Home/handyman/pest-control-services/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import NavBar from "@/app/Components/Navabar";
+import Footer from "@/app/Components/HandymanFooter";
+import HeroSection from "@/app/Components/PestControlBanner"
+import PestControlServices from "@/app/Components/PestControlService";
+import PestControlServiceDescription from "@/app/Components/PestControlDescription";
+export default function HandymanHome() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <HeroSection></HeroSection>
+      <PestControlServices></PestControlServices>
+      <PestControlServiceDescription></PestControlServiceDescription>
+      <Footer></Footer>
+
+    </div>
+  );
+}
