433eb66381cd0cf734ff53e865f7880584d94d3c
Faizan80235 <fa3031980@gmail.com>
2025-11-13 17:44:27 +0500

    Updated Curtain Cleaning Services page – added component

--- src/app/(pages)/Home/curtain-cleaning-services/page.tsx
+++ src/app/(pages)/Home/curtain-cleaning-services/page.tsx
@@ -4,11 +4,13 @@ import NavBar from "@/app/Components/CleaningServiceNavbar";
 import CurtainCleaningBanner from "@/app/Components/curtain_cleaning_banner";
 import CurtainCleaningDescription from "@/app/Components/curtain_cleaning_description";
 import Footer from "@/app/Components/HandymanFooter";
+import CurtainCleaningServiceCard from "@/app/Components/CurtainServiceCard"
 export default function HandymanHome() {
   return (
     <div>
       <NavBar></NavBar>
       <CurtainCleaningBanner></CurtainCleaningBanner>
+      <CurtainCleaningServiceCard></CurtainCleaningServiceCard>
       <CurtainCleaningDescription></CurtainCleaningDescription>
       <Footer></Footer>
     </div>
