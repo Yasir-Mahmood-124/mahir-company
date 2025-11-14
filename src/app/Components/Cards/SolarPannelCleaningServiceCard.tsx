998b91b1578b584a3d0ea10dbbc83ebfd84c7fad
Faizan80235 <fa3031980@gmail.com>
2025-11-15 03:51:18 +0500

    Updated Solar Panel Cleaning Service Card

--- src/app/Components/Cards/SolarPannelCleaningServiceCard.tsx
+++ src/app/Components/Cards/SolarPannelCleaningServiceCard.tsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import { Container, Typography, Box, Grid } from '@mui/material';
-import ServiceCard from '../ServiceCard';
+import ServiceCard from '../Card_Details/CleaningServicesCardDetails';
 
 interface SolarPanelCleaningServicesSectionProps {
   serviceIds?: number[];
