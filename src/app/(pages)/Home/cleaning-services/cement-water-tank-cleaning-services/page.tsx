538359694dc7f804f17e46f3042709800a5f8a77
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:33 +0500

    Updated cement-water-tank-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/cement-water-tank-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/cement-water-tank-cleaning-services/page.tsx
@@ -3,8 +3,8 @@
 import CementTankCleaningServicesSection from "@/app/Components/Cards/CementTankServicecard";
 import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
-import CementTankCleaningBanner from "@/app/Components/cement_tank_cleaning_banner";
-import CementTankCleaningDescription from "@/app/Components/cement_tank_description";
+import CementTankCleaningBanner from "@/app/Components/banners/cement_tank_cleaning_banner";
+import CementTankCleaningDescription from "@/app/Components/Cards_Description/cement_tank_description";
 export default function HandymanHome() {
   return (
     <div>
