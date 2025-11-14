aaf78bed839ed968b01986c49a5b15550b1c66fe
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:53:36 +0500

    Updated painter-services page

--- src/app/(pages)/Home/handyman/painter-services/page.tsx
+++ src/app/(pages)/Home/handyman/painter-services/page.tsx
@@ -2,9 +2,9 @@
 
 import NavBar from "@/app/Components/Navabar";
 import Footer from "@/app/Components/HandymanFooter";
-import HeroSection from "@/app/Components/PainterservicesBanner";
+import HeroSection from "@/app/Components/banners/PainterservicesBanner";
 import PainterService from "@/app/Components/Cards/PainterServiceSectionCard";
-import PainterServiceDescription from "@/app/Components/PainterServiceDescription";
+import PainterServiceDescription from "@/app/Components/Cards_Description/PainterServiceDescription";
 export default function HandymanHome() {
   return (
     <div>
