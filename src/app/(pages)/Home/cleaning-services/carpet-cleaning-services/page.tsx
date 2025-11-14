a04ad657df1c587ed14c960732b25337c0a3f6ad
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:30 +0500

    Updated carpet-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/carpet-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/carpet-cleaning-services/page.tsx
@@ -3,8 +3,8 @@
 import CarpetCleaningServicesSection from "@/app/Components/Cards/CarpetCleaningServicesCard";
 import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
-import CarpetCleaningBanner from "@/app/Components/carpet_cleaning_banner";
-import CarpetCleaningDescription from "@/app/Components/carpet_cleaning_description";
+import CarpetCleaningBanner from "@/app/Components/banners/carpet_cleaning_banner";
+import CarpetCleaningDescription from "@/app/Components/Cards_Description/carpet_cleaning_description";
 export default function HandymanHome() {
   return (
     <div>
