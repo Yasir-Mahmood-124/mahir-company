2117f36e6d01d2a2fc0ec96735a527efc9398d0b
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:41 +0500

    Updated commercial-deep-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/commercial-deep-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/commercial-deep-cleaning-services/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import NavBar from "@/app/Components/CleaningServiceNavbar";
-import CommercialDeepCleaningBanner from "@/app/Components/commercial_deep_cleaning_banner";
-import CommercialDeepCleaningDescription from "@/app/Components/commercial_deep_cleaning_desc";
+import CommercialDeepCleaningBanner from "@/app/Components/banners/commercial_deep_cleaning_banner";
+import CommercialDeepCleaningDescription from "@/app/Components/Cards_Description/commercial_deep_cleaning_desc";
 import CommercialDeepCleaningServicesSection from "@/app/Components/Cards/CommericlDeepCleaningCard";
 import Footer from "@/app/Components/HandymanFooter";
 export default function HandymanHome() {
