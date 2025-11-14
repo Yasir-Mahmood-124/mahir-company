a386562ffe1377665c3511f63d6f70d76c2a830f
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:51 +0500

    Updated mattress-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/mattress-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/mattress-cleaning-services/page.tsx
@@ -3,8 +3,8 @@
 import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
 import MattresCleaningService from "@/app/Components/Cards/MattresCLeaningServiceCard";
-import MattressCleaningBanner from "@/app/Components/mattress_cleaning_banner";
-import MattressCleaningDescription from "@/app/Components/mattress_cleaning_description";
+import MattressCleaningBanner from "@/app/Components/banners/mattress_cleaning_banner";
+import MattressCleaningDescription from "@/app/Components/Cards_Description/mattress_cleaning_description";
 export default function HandymanHome() {
   return (
     <div>
