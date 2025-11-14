8730a93815bbe0e11eaeea6fe2b1c287f18d2f9e
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:58 +0500

    Updated plastic-water-tank-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/plastic-water-tank-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/plastic-water-tank-cleaning-services/page.tsx
@@ -2,8 +2,8 @@
 
 import NavBar from "@/app/Components/CleaningServiceNavbar";
 import Footer from "@/app/Components/HandymanFooter";
-import PlasticTankCleaningBanner from "@/app/Components/plastic_tank_cleaning_banner";
-import PlasticTankCleaningDescription from "@/app/Components/plastic_tank_description";
+import PlasticTankCleaningBanner from "@/app/Components/banners/plastic_tank_cleaning_banner";
+import PlasticTankCleaningDescription from "@/app/Components/Cards_Description/plastic_tank_description";
 import WaterTankCleaningServicesSection from "@/app/Components/Cards/WaterTankCleaningServiceCard";
 export default function HandymanHome() {
   return (
