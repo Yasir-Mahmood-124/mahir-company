f45284de88559208ac40547ba04c232ee65a8f4a
Faizan80235 <fa3031980@gmail.com>
2025-11-15 03:51:00 +0500

    Updated Handyman AC Service page

--- src/app/(pages)/Home/handyman/ac-services/page.tsx
+++ src/app/(pages)/Home/handyman/ac-services/page.tsx
@@ -4,7 +4,7 @@ import NavBar from "@/app/Components/Navabar";
 import Footer from "@/app/Components/HandymanFooter";
 import BannerSection from "@/app/Components/banners/Banner";
 import HeroSection from "@/app/Components/banners/AcServiceBanner";
-import ServiceCard from "@/app/Components/ServiceCard";
+import ServiceCard from "@/app/Components/Card_Details/HandymanServiceCardDetails";
 import ACServicesSection from "@/app/Components/Cards/AcServiceSectionCard";
 import ACServicesDescription from "@/app/Components/Cards_Description/ACServicesDescription";
 export default function HandymanHome() {
