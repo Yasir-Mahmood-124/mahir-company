6821be8f4e467d37d27ae8dd6f54be7cc9205c70
Faizan80235 <fa3031980@gmail.com>
2025-11-15 03:31:47 +0500

    Add hair-treatment-services pages

---
+++ src/app/(pages)/Home/beautician/hair-treatment-services/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import NavBar from "@/app/Components/beauticianNavabr";
+
+import Footer from "@/app/Components/BeauticianFooter";
+import CopyrightSection from "@/app/Components/CopyrightSection";
+import Banner from "@/app/Components/banners/hair-treatment-banner";
+export default function Home() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Banner></Banner>
+      <br></br>
+      <br></br>
+      <Footer></Footer>
+      <CopyrightSection></CopyrightSection>
+    </div>
+  );
+}
