9e93c1d8de2367243343da2495e71dbc0338b1c0
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:52:48 +0500

    Updated deep-cleaning-services page

--- src/app/(pages)/Home/cleaning-services/deep-cleaning-services/page.tsx
+++ src/app/(pages)/Home/cleaning-services/deep-cleaning-services/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import NavBar from "@/app/Components/CleaningServiceNavbar";
-import DeepCleaningBanner from "@/app/Components/deep_cleaning_banner";
-import DeepCleaningDescription from "@/app/Components/deep_cleaning_description";
-import DeepCleaningServicesSection from "@/app/Components/DeepCleaningServiceCard";
+import DeepCleaningBanner from "@/app/Components/banners/deep_cleaning_banner";
+import DeepCleaningDescription from "@/app/Components/Cards_Description/deep_cleaning_description";
+import DeepCleaningServicesSection from "@/app/Components/Cards/DeepCleaningServiceCard";
 import Footer from "@/app/Components/HandymanFooter";
 export default function HandymanHome() {
   return (
