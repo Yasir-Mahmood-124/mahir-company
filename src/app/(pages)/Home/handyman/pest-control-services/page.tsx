3f60c035e8bd24b0a1b9f8cb6acf720e4c1a32b2
Faizan80235 <fa3031980@gmail.com>
2025-11-15 02:53:40 +0500

    Updated pest-control-services page

--- src/app/(pages)/Home/handyman/pest-control-services/page.tsx
+++ src/app/(pages)/Home/handyman/pest-control-services/page.tsx
@@ -2,9 +2,9 @@
 
 import NavBar from "@/app/Components/Navabar";
 import Footer from "@/app/Components/HandymanFooter";
-import HeroSection from "@/app/Components/PestControlBanner"
+import HeroSection from "@/app/Components/banners/PestControlBanner"
 import PestControlServices from "@/app/Components/Cards/PestControlServiceCard";
-import PestControlServiceDescription from "@/app/Components/PestControlDescription";
+import PestControlServiceDescription from "@/app/Components/Cards_Description/PestControlDescription";
 export default function HandymanHome() {
   return (
     <div>
