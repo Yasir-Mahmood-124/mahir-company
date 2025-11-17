5d7dbbae3ef57f6dc14a571b53547fa300819e37
Faizan80235 <fa3031980@gmail.com>
2025-11-18 01:59:23 +0500

    Update mehndi services page

--- src/app/(pages)/Home/beautician/mehndi-services/page.tsx
+++ src/app/(pages)/Home/beautician/mehndi-services/page.tsx
@@ -6,13 +6,13 @@ import NavBar from "@/Components/beauticianNavabr";
 import Footer from "@/Components/BeauticianFooter";
 import CopyrightSection from "@/Components/CopyrightSection";
 import Banner from "@/Components/banners/mehndi-services-banner";
+import MehndiService from "@/Components/Cards/MehndiServiceCard";
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Banner></Banner>
-      <br></br>
-      <br></br>
+      <MehndiService></MehndiService>
       <Footer></Footer>
       <CopyrightSection></CopyrightSection>
     </div>
