6c3951b11dfa6adbbc7c698ccdafcc3bf64ac38c
Faizan80235 <fa3031980@gmail.com>
2025-11-18 01:57:26 +0500

    Update hair styling and hair cut page

--- src/app/(pages)/Home/beautician/hair-styling-and-hair-cut/page.tsx
+++ src/app/(pages)/Home/beautician/hair-styling-and-hair-cut/page.tsx
@@ -6,13 +6,13 @@ import NavBar from "@/Components/beauticianNavabr";
 import Footer from "@/Components/BeauticianFooter";
 import CopyrightSection from "@/Components/CopyrightSection";
 import Hairstylingbanner from "@/Components/banners/hair-styling-banner";
+import HairStylingCard from "@/Components/Cards/HairStylingCard"
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Hairstylingbanner></Hairstylingbanner>
-      <br></br>
-      <br></br>
+      <HairStylingCard></HairStylingCard>
       <Footer></Footer>
       <CopyrightSection></CopyrightSection>
     </div>
