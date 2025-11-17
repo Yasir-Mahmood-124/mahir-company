70254be59cabc9bde49ff5a1eca5f7768965a6b3
Faizan80235 <fa3031980@gmail.com>
2025-11-18 01:59:30 +0500

    Update monthly beauty deals page

--- src/app/(pages)/Home/beautician/monthly-beauty-deals/page.tsx
+++ src/app/(pages)/Home/beautician/monthly-beauty-deals/page.tsx
@@ -6,13 +6,13 @@ import NavBar from "@/Components/beauticianNavabr";
 import Footer from "@/Components/BeauticianFooter";
 import CopyrightSection from "@/Components/CopyrightSection";
 import Banner from "@/Components/banners/monthly-beauty-banner";
+import MonthlyBeautyDealsCard from "@/Components/Cards/MonthlyBeautyDeals";
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Banner></Banner>
-      <br></br>
-      <br></br>
+      <MonthlyBeautyDealsCard></MonthlyBeautyDealsCard>
       <Footer></Footer>
       <CopyrightSection></CopyrightSection>
     </div>
