2e16a00aa7baea621f3fe7422f0db428c474bed2
Faizan80235 <fa3031980@gmail.com>
2025-11-18 01:59:37 +0500

    Update salon packages page

--- src/app/(pages)/Home/beautician/salon-packages/page.tsx
+++ src/app/(pages)/Home/beautician/salon-packages/page.tsx
@@ -6,13 +6,13 @@ import NavBar from "@/Components/beauticianNavabr";
 import Footer from "@/Components/BeauticianFooter";
 import CopyrightSection from "@/Components/CopyrightSection";
 import Banner from "@/Components/banners/salon-packages-banner";
+import SalonServiceCard from "@/Components/Cards/SalonServiceCard"
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Banner></Banner>
-      <br></br>
-      <br></br>
+      <SalonServiceCard></SalonServiceCard>
       <Footer></Footer>
       <CopyrightSection></CopyrightSection>
     </div>
