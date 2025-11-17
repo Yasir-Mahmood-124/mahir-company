fc0535b88afa5d3b13a8b65b9600ca2b547d8589
Faizan80235 <fa3031980@gmail.com>
2025-11-18 01:59:42 +0500

    Update waxing services page

--- src/app/(pages)/Home/beautician/waxing-services/page.tsx
+++ src/app/(pages)/Home/beautician/waxing-services/page.tsx
@@ -6,13 +6,13 @@ import NavBar from "@/Components/beauticianNavabr";
 import Footer from "@/Components/BeauticianFooter";
 import CopyrightSection from "@/Components/CopyrightSection";
 import Banner from "@/Components/banners/waxing-services-banner";
+import WaxingService from "@/Components/Cards/WaxingServiceCard"
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Banner></Banner>
-      <br></br>
-      <br></br>
+      <WaxingService></WaxingService>
       <Footer></Footer>
       <CopyrightSection></CopyrightSection>
     </div>
